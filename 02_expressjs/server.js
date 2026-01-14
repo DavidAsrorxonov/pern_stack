e76f54f82227ac0fb1331935aff7a2a8f3fc5306
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-14 13:24:59 +0900

    add example APIs

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -7,6 +7,26 @@ app.get("/", (req, res) => {
   res.send("Hello from the Express server");
 });
 
+app.get("/api/v1/cars", (req, res) => {
+  res.send("All cars");
+});
+
+app.post("/api/v1/cars", (req, res) => {
+  res.send("New car");
+});
+
+app.put("/api/v1/cars/:id", (req, res) => {
+  res.send(`Update car ${req.params.id}`);
+});
+
+app.delete("/api/v1/cars/:id", (req, res) => {
+  res.send(`Delete car ${req.params.id}`);
+});
+
+app.get("/api/v1/cars/:id", (req, res) => {
+  res.send(`Get car ${req.params.id}`);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
